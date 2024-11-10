cfc8c7537709a7d7ddda79d228f89b52ccf0f199
KainoVaii <tom@baron-bourgogne.fr>
2024-11-10 15:02:30 +0100

    🛠️ Fix (Add alpine file)

--- app/View/layouts/main.layout.php
+++ app/View/layouts/main.layout.php
@@ -6,9 +6,8 @@
     <title>Mon blog</title>
     <link title="default" rel="stylesheet" href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <script defer src="https://unpkg.com/alpinejs-component@latest/dist/component.min.js"></script>
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script> window.xComponent = { name: 'js-component', }</script>
+    <script defer src="/alpine/component.min.js"></script>
+    <script defer src="/alpine/alpine.min.js"></script>
 </head>
 <body>
     {{header}}
