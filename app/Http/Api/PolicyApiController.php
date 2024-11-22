04c01dbb2d314acdceccdbb96afae3584dd774b6
KainoVaii <tom@baron-bourgogne.fr>
2024-11-22 22:51:51 +0100

    📦️ Update (update)

--- app/Http/Api/PolicyApiController.php
+++ app/Http/Api/PolicyApiController.php
@@ -10,12 +10,24 @@ class PolicyApiController extends Controller
     #[Route('/api/policies/edit', 'POST', 'auth')]
     public function edit(Request $_request): void
     {
-        $this->container->policyService->interact($_request->getBody()['name']);
-        $this->container->policyService->update([
-            'name' => $_request->getBody()['name'],
-        ]);
+        $formData = $_request->getBody();
+        $perms = array_splice($formData, 1);
+
+        foreach ($perms as $key => $perm)
+        {
+            if ($perm==="true")
+            {
+                $data[$key] = true;
+            } else {
+                $data[$key] = false;
+            }
+        }
+
+        $this->container->policyService->interact($formData['policy_name']);
+        $this->container->policyService->update($data);
 
         $this->container->flash->success("La policy a bien été modifié");
-        $this->redirect('/policies/'.$_request->getBody()['name'].'/edit');
+        $this->redirect('/settings/policy/'.$formData['policy_name']);
+
     }
 }
